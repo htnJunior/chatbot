e7fa4053cab0869a53854f501a6d5bb4d2d38588
htnJunior <junioor_souzaa282@hotmail.com>
2021-04-21 18:38:39 -0300

    client-server-setups

--- server/src/index.js
+++ server/src/index.js
@@ -1,13 +1,40 @@
 const express = require('express')
 const socketio = require('socket.io')
 const http = require('http')
-const PORT = process.env.PORT || 5000
 const router = require('./router')
+//const cors = require('cors')
+
+const { addUser, removeUser, getUser, getUsersInRoom } = require('./users')
+
 const app = express()
 const server = http.createServer(app)
+const PORT = process.env.PORT || 5000
+const io = socketio(server)
 
 app.use(router)
 
-const io = socketio(server)
+io.on('connection', (socket) => {
+  socket.on('join', ({ name, room }, callback) => {
+    const { error, user } = addUser({ id: socket.id, name, room })
+
+    if (error) return callback(error)
+
+    socket.emit('message', {
+      user: 'admin',
+      text: `${user.name}, welcome to the room ${user.room}`,
+    })
+    socket.broadcast
+      .to(user.room)
+      .emit('message', { user: 'admin', text: `${user.name}, has joined!` })
+
+    socket.join(user.room)
+
+    callback()
+  })
+
+  socket.on('disconnect', () => {
+    console.log('User had left')
+  })
+})
 
 server.listen(PORT, () => console.log(`Server running at port ${PORT}`))
